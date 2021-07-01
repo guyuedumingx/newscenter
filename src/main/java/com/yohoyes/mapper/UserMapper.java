4733761e7935a762e92318d4cebb7f835c89653d
guyuedumingx <guyuedumingx@gmail.com>
2021-07-01 11:41:46 +0800

    :alien: finish UserMapper

--- src/main/java/com/yohoyes/mapper/UserMapper.java
+++ src/main/java/com/yohoyes/mapper/UserMapper.java
@@ -1,10 +1,8 @@
 package com.yohoyes.mapper;
 
 import com.yohoyes.pojo.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 /**
@@ -18,11 +16,19 @@ public interface UserMapper {
     @Select("select * from t_user")
     List<User> queryUserList();
 
+    @Select("select * from t_user where id=#{id}")
     User queryUserById(int id);
 
+    @Insert("insert into t_user values (null, #{name},#{email},#{pwd}")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     int addUser(User user);
 
+    @Update("update t_user set name=#{name}, email=#{email}, pwd=#{pwd} where id=#{id}")
     int updateUser(User user);
 
+    @Delete("delete from t_user where id=#{id}")
     int deleteUser(int id);
+
+    @Select("select * from t_user where email=#{email} and pwd=#{pwd}")
+    User login(@Param("email") String email,@Param("pwd") String pwd);
 }
