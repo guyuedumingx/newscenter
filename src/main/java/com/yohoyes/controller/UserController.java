8baee14a8ed67befcd4ba66ac4caf89fe19edeea
guyuedumingx <guyuedumingx@gmail.com>
2021-07-01 11:53:14 +0800

    :alien: fixed User query bug

--- src/main/java/com/yohoyes/controller/UserController.java
+++ src/main/java/com/yohoyes/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.List;
 
 /**
@@ -18,10 +19,20 @@ public class UserController {
     @Autowired
     UserMapper userMapper = null;
 
-    @RequestMapping("/user")
+    @RequestMapping("/user/list")
     public String getAllUser() {
         List<User> users = userMapper.queryUserList();
         return users.toString();
+    }
+
+    @RequestMapping("/user/add")
+    public String addUser() {
+        User user = new User();
+        user.setEmail("111@qq.com");
+        user.setName("ww2");
+        user.setPassword("111");
+        userMapper.addUser(user);
+        return user.toString();
     }
 
 }
--- src/main/java/com/yohoyes/mapper/UserMapper.java
+++ src/main/java/com/yohoyes/mapper/UserMapper.java
@@ -19,16 +19,16 @@ public interface UserMapper {
     @Select("select * from t_user where id=#{id}")
     User queryUserById(int id);
 
-    @Insert("insert into t_user values (null, #{name},#{email},#{pwd}")
+    @Insert("insert into t_user (name,email,password) values (#{name},#{email},#{password})")
     @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     int addUser(User user);
 
-    @Update("update t_user set name=#{name}, email=#{email}, pwd=#{pwd} where id=#{id}")
+    @Update("update t_user set name=#{name}, email=#{email}, password=#{password} where id=#{id}")
     int updateUser(User user);
 
     @Delete("delete from t_user where id=#{id}")
     int deleteUser(int id);
 
-    @Select("select * from t_user where email=#{email} and pwd=#{pwd}")
-    User login(@Param("email") String email,@Param("pwd") String pwd);
+    @Select("select * from t_user where email=#{email} and password=#{password}")
+    User login(@Param("email") String email,@Param("password") String pwd);
 }
