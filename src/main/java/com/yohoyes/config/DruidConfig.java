9407f4feb86ff189729bb720e88e7f1f66c8925e
guyuedumingx <guyuedumingx@gmail.com>
2021-07-01 11:04:59 +0800

    :rocket: add druid config and log4j config

---
+++ src/main/java/com/yohoyes/config/DruidConfig.java
@@ -0,0 +1,42 @@
+package com.yohoyes.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author yohoyes
+ * @date 2021/7/1 10:43
+ */
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix="spring.datasource")
+    @Bean
+    public DataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    /**
+     * 后台监控
+     */
+    @Bean
+    public ServletRegistrationBean a() {
+        ServletRegistrationBean<StatViewServlet> bean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        Map<String, String> initMap =  new HashMap<>();
+
+        initMap.put("loginUsername", "admin");
+        initMap.put("loginPassword", "111");
+
+        initMap.put("allow", "");
+
+        bean.setInitParameters(initMap);
+        return bean;
+    }
+}
--- src/test/java/com/yohoyes/NewscenterApplicationTests.java
+++ src/test/java/com/yohoyes/NewscenterApplicationTests.java
@@ -1,10 +1,16 @@
 package com.yohoyes;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class NewscenterApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
 
     @Test
     void contextLoads() {
