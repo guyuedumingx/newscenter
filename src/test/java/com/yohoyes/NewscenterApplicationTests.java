11abc5da2db54488cb1e1690f0f1abed8464d3a9
guyuedumingx <guyuedumingx@gmail.com>
2021-07-01 10:21:07 +0800

    :rocket: first commit

---
+++ src/test/java/com/yohoyes/NewscenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yohoyes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewscenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
